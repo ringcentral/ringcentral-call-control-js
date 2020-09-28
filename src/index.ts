c484f5bcac29744f3883460876760dc1259e95fc
jackson lin <jackson.lin@ringcentral.com>
2020-09-28 10:55:47 +0800

    export eventTime as creationTime

--- src/index.ts
+++ src/index.ts
@@ -137,7 +137,7 @@ export class RingCentralCallControl extends EventEmitter {
     if (message.event.indexOf('/telephony/sessions') === -1) {
       return;
     }
-    const { telephonySessionId, ...newData } = message.body;
+    const { eventTime, telephonySessionId, ...newData } = message.body;
     if (!telephonySessionId) {
       return;
     }
@@ -146,6 +146,7 @@ export class RingCentralCallControl extends EventEmitter {
     newData.extensionId = this.extensionId;
     newData.accountId = this.accountId;
     newData.parties = newData.parties.map(p => formatParty(p));
+    newData.creationTime = eventTime;
     if (!existedSession) {
       const disconnectedParties = newData.parties.filter(p => p.status.code === 'Disconnected');
       if (disconnectedParties.length === newData.parties.length) {
